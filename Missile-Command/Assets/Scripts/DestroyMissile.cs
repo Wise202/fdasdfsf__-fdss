b08cc496ffe05d1668c8ab5e0f0a1f4fd1b7db5b
Wise202 <51159440+Wise202@users.noreply.github.com>
2019-06-26 14:58:57 +0800

    1
    1

---
+++ Missile-Command/Assets/Scripts/DestroyMissile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyMissile : MonoBehaviour
+{
+    public Vector3 mousePos;
+    public Vector3 deathPos;
+    // Start is called before the first frame update
+    void Awake()
+    {
+       // deathPos = transform.position;
+        mousePos = Input.mousePosition;
+        transform.position = mousePos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Explosion_Point")
+        {
+            Debug.Log("Hey");
+        }
+    }
+
+}
---
+++ Missile-Command/Assets/Scripts/P1Controller.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1Controller : MonoBehaviour
+{
+    public Rigidbody missilePrefab;
+    public Transform cannonTop;
+    public Vector3 mousePos;
+    private bool mouseClicked;
+    public GameObject missileDestroy;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        LookAtMouse();
+        FireMissile();
+    }
+
+    void LookAtMouse()
+    {
+        var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+    
+    void FireMissile()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Rigidbody missileInstance;
+            missileInstance = Instantiate(missilePrefab, cannonTop.position, cannonTop.rotation) as Rigidbody;
+            missileInstance.AddForce(cannonTop.forward * 3000);
+        }
+        if (Input.GetMouseButtonDown(0) && mouseClicked == false)
+        {
+            Instantiate(missileDestroy);
+            mouseClicked = true;
+            mousePos = Input.mousePosition;
+        }
+        if (Input.GetMouseButtonUp(0) && mouseClicked == true)
+        {
+            mouseClicked = false;
+        }
+    }
+}
