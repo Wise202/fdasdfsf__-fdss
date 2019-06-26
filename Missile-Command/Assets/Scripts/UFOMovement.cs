5fa8d9dc54d7c6ff271395864ec5bafe578b0d23
Khokolate <48821011+Khokolate@users.noreply.github.com>
2019-06-26 20:08:58 +0800

    UFO

---
+++ Missile-Command/Assets/Scripts/UFOMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOMovement : MonoBehaviour
+{
+    public bool goLeft;
+
+    public float force; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+    }
+
+    private void FixedUpdate()
+    {
+        MoveUFO();
+    }
+
+    void MoveUFO()
+    {
+        if(goLeft == true)
+        {
+            GetComponent<Rigidbody>().AddForce(-transform.right * force);
+        }
+
+        else if(goLeft == false)
+        {
+            GetComponent<Rigidbody>().AddForce(transform.right * force);
+        }
+    }
+}
