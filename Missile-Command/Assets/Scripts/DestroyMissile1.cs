1e4034a525b7300ab9cc227eafbdf9f40df0b232
Guillaume180 <43747099+Guillaume180@users.noreply.github.com>
2019-07-02 21:48:24 +0800

    add on

---
+++ Missile-Command/Assets/Scripts/DestroyMissile1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyMissile1 : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
