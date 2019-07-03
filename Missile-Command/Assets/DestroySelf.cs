8c5dadb039e12d697c9751c382df21c82269c32e
Wildtigger <51219988+Wildtigger@users.noreply.github.com>
2019-07-03 14:47:14 +0800

    Missile target fixing

---
+++ Missile-Command/Assets/DestroySelf.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelf : MonoBehaviour
+{
+    public float deathTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deathTime += Time.deltaTime;
+        if (deathTime > 3.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Missile")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
