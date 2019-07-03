e3b657cf310d335acec9a349ff96d584a0b9fd09
Guillaume180 <43747099+Guillaume180@users.noreply.github.com>
2019-07-03 18:02:21 +0800

    Change

---
+++ Missile-Command/Assets/Scripts/WaveScript.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveScript : MonoBehaviour
+{
+    //This enum will make sure that the wave will work in the order it meant to go.
+    public enum SpawnState { Spawning, Waiting, Countdown };
+    private SpawnState state = SpawnState.Countdown;
+
+    //Setting up the how each wave will work.
+    public class EnemyWave
+    {
+        public string name;
+        public Transform[] enemy;
+        public int count;
+        public float rate;
+    }
+
+    public EnemyWave[] waves;
+    private int nextWave = 0;
+    public int wavenumber = 0;
+
+    public Transform[] enemySpawnPoint;
+
+    //The time it will take for when the wave ends to the next wave.
+    public float enemySpawnDelay;
+    public float enemySpawnCountDown;
+    public float searchCountDown = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (enemySpawnPoint.Length == 0)
+        {
+            Debug.LogError("No spawn points reference!!");
+
+            enemySpawnCountDown = enemySpawnDelay;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == SpawnState.Waiting)
+        {
+            //This is to check that all the enemy missile is gone.
+            if (!EnemyIsAlive())
+            {
+                NewWave();
+                return;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    bool EnemyIsAlive()
+    {
+        searchCountDown -= Time.deltaTime;
+        if (searchCountDown <= 0f)
+        {
+            searchCountDown = 1f;
+            if (GameObject.FindGameObjectsWithTag("EnemyMissile").Length == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void NewWave()
+    {
+        Debug.Log("All Missile Gone. Wave Complete");
+
+        state = SpawnState.Countdown;
+        enemySpawnCountDown = enemySpawnDelay;
+
+        if (nextWave + 1 > waves.Length - 1)
+        {
+            nextWave = 0;
+            Debug.Log("All waves completed! Time to Loop");
+        }
+        else
+        {
+            nextWave++;
+        }
+    }
+
+    IEnumerator SpawningMissiles(EnemyWave wave)
+    {
+        Debug.Log("Spawning new wave " + wave.name);
+
+        state = SpawnState.Spawning;
+
+        for (int i = 0; i < wave.count; i++)
+        {
+            EnemyWave missile = waves[i];
+            SpawnMissile(missile.enemy[Random.Range(0, missile.enemy.Length)]);
+            yield return new WaitForSeconds(1f / wave.rate);
+            SpawnMissile(missile.enemy[Random.Range(0, missile.enemy.Length)]);
+            yield return new WaitForSeconds(1.5f / wave.rate);
+            SpawnMissile(missile.enemy[Random.Range(0, missile.enemy.Length)]);
+            yield return new WaitForSeconds(2f / wave.rate);
+        }
+
+        state = SpawnState.Waiting;
+
+        yield break;
+    }
+
+    void SpawnMissile(Transform missile)
+    {
+
+    }
+}
